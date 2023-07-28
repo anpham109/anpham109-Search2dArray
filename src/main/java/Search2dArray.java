c8910b7d6d92a20a895964b86e68919022f68148
Andy Pham <anpham-work@protonmail.com>
2023-07-28 15:21:13 +0000

    finished the challenge

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for (int[] is : nums) {
+            for (int is2 : is) {
+                if(is2 == target){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
